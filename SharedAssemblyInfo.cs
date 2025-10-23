cf8942ac13359bcaacfb3ddf19f966b1029eac27
James A Sutherland <j@sutherland.pw>
2025-10-22 21:15:37 -0500

    Bump version to 4.2.0
    Update version numbers in:
    - Directory.Build.props
    - SharedAssemblyInfo.cs
    - CHANGELOG.md
    
    Release notes include:
    - Migration to FAnsiSql.Legacy 3.3.1 and .NET 9.0
    - Build configuration centralization
    - Performance optimizations
    - MG modality template
    - Bug fixes and analyzer warning resolutions

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Universities of Dundee and Edinburgh")]
 [assembly: AssemblyProduct("DicomTypeTranslation")]
-[assembly: AssemblyCopyright("Copyright © 2020-2024")]
+[assembly: AssemblyCopyright("Copyright © 2020-2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // These should be overwritten by release builds
-[assembly: AssemblyVersion("4.1.5")]
-[assembly: AssemblyFileVersion("4.1.5")]
-[assembly: AssemblyInformationalVersion("4.1.5")]
+[assembly: AssemblyVersion("4.2.0")]
+[assembly: AssemblyFileVersion("4.2.0")]
+[assembly: AssemblyInformationalVersion("4.2.0")]
